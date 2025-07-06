0aede2fe7853d5d038b91dbcdb72218890b2826a
alimotamed <am.frx74@gmail.com>
2025-07-06 11:44:01 +0330

    edit utility

--- utility.py
+++ utility.py
@@ -1,12 +1,12 @@
 def convert_price(price):
     if price.text.strip() == 'توافقی':
-        price = price.text.strip()
+        return price.text.strip()
     else:
-        price = float(price.text.replace(',', ''))
+        return float(price.text.replace(',', ''))
 
 
 def convert_description(description):
     if description:
-        description = description.text
+        return description.text
     else:
-        description = ''
+        return ''
